68c43d469c1603d2743411be68fe7d0bc948f6ce
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-10 08:41:09 +0000

    Deploy EdisonJwa/Donate to github.com/EdisonJwa/Donate.git:master

---
+++ precache-manifest.f77d7dbfe07232e1f0f6635b33b4e011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42182e577c9a1b853a40f04ab24f6335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "052d8d1610f1aae5965a",
+    "url": "/static/css/2.0b5d44dd.chunk.css"
+  },
+  {
+    "revision": "ff0db8eb52c4c623a7c5",
+    "url": "/static/css/main.ddbfc930.chunk.css"
+  },
+  {
+    "revision": "052d8d1610f1aae5965a",
+    "url": "/static/js/2.01911973.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.01911973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0db8eb52c4c623a7c5",
+    "url": "/static/js/main.142cb1df.chunk.js"
+  },
+  {
+    "revision": "fd84a6d6aa3946c7a2a8",
+    "url": "/static/js/runtime-main.b1818e05.js"
+  }
+]);
